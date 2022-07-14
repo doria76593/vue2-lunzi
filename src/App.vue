358294c7da59a74981a7234a2d63e9adffd6d6ac
doria0421 <441419849@qq.com>
2022-07-14 09:40:15 +0800

    完成按钮基本样式

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app">app
+  <div id="app">
+    <h2>按钮</h2>
+    <g-button></g-button>
   </div>
 </template>
 
 <script>
-
+import Button from './button.vue'
 export default {
-
+  components: {
+    'g-button': Button,
+  },
 }
 </script>
 
