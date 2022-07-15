c96725438938c5b0638526532aa52c45422ece1b
doria0421 <441419849@qq.com>
2022-07-15 11:34:55 +0800

    添加name

--- src/Icon.vue
+++ src/Icon.vue
@@ -7,6 +7,7 @@
 <script>
 import './svg'
 export default {
+  name:'GIcon',
   props: ['name'],
 }
 </script>
--- src/button-group.vue
+++ src/button-group.vue
@@ -6,6 +6,7 @@
 
 <script>
 export default {
+  name:'GButtonGroup',
   data() {
     return {}
   },
