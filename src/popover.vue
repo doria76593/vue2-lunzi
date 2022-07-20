b2fd49b41bf6a2f8267525c833aa3dff79338dcc
doria0421 <441419849@qq.com>
2022-07-20 11:30:32 +0800

    初步完成popover

--- src/popover.vue
+++ src/popover.vue
@@ -4,13 +4,14 @@
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
-      <slot></slot>
+      <slot></slot>{{visible}}
     </span>
 
   </div>
 </template>
 
 <script>
+// 思路：1-通过两个node.contains来判断是否触发事件
 export default {
   name: 'GPopover',
   data() {
@@ -19,11 +20,41 @@ export default {
   methods: {
     close() {
       this.visible = false
-      // console.log('document 隐藏 popover')
-      // document.removeEventListener('click', this.onClickDocument)
+      document.removeEventListener('click', this.onClickDocument)
     },
     open() {
       this.visible = true
+      this.$nextTick(() => {
+        this.positionContent()
+        document.addEventListener('click', this.onClickDocument)
+      })
+    },
+
+    onClickDocument(e) {
+      let popover = this.$refs.popover
+      if (e.target.contains(popover)) {
+        this.close()
+      }
+    },
+    positionContent() {
+      let contentWrapper = this.$refs.contentWrapper
+      let popover = this.$refs.popover
+      let { left, top } = popover.getBoundingClientRect()
+
+      document.body.appendChild(contentWrapper)
+      let scroll = this.getScroll()
+      contentWrapper.style.left = `${scroll.x + left}px`
+      contentWrapper.style.top = `${scroll.y + top}px`
+    },
+    onClick(ev) {
+      let bool = this.$refs.triggerWrapper.contains(ev.target)
+      if (bool) {
+        if (this.visible === false) {
+          this.open()
+        } else {
+          this.close()
+        }
+      }
     },
     getScroll() {
       var x =
@@ -45,30 +76,6 @@ export default {
       return {
         x,
         y,
-      }
-    },
-    onClickDocument() {},
-    positionContent() {
-      let contentWrapper = this.$refs.contentWrapper
-      let popover = this.$refs.popover
-      let { left, top } = popover.getBoundingClientRect()
-
-      document.body.appendChild(contentWrapper)
-      let scroll = this.getScroll()
-      contentWrapper.style.left = `${scroll.x + left}px`
-      contentWrapper.style.top = `${scroll.y + top}px`
-    },
-    onClick(ev) {
-      console.log('ev,,,', ev)
-      this.visible = !this.visible
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          // this.close()
-          this.positionContent()
-          // document.addEventListener('click', this.onClickDocument)
-        })
-      } else {
-        // console.log('vm 隐藏 popover')
       }
     },
   },
