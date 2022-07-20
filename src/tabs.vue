aa61d1843193b453f63298b5b4f438a7fa4471fe
doria0421 <441419849@qq.com>
2022-07-20 11:40:39 +0800

    重构 tabs.vue[每一个函数，优化到5行左右，就没有优化的空间了]

--- src/tabs.vue
+++ src/tabs.vue
@@ -30,24 +30,34 @@ export default {
       eventBus: new Vue(),
     }
   },
-  mounted() {
-    if (this.$children.length === 0) {
-      console &&
-        console.warn &&
-        console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === 'GTabsHead') {
-        vm.$children.forEach((childVm) => {
-          if (
-            childVm.$options.name === 'GTabsItem' &&
-            childVm.name === this.selected
-          ) {
-            this.eventBus.$emit('update:selected', this.selected, childVm)
-          }
-        })
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console &&
+          console.warn &&
+          console.warn(
+            'tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件'
+          )
       }
-    })
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'GTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (
+              childVm.$options.name === 'GTabsItem' &&
+              childVm.name === this.selected
+            ) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
+    },
+  },
+  mounted() {
+    this.checkChildren()
+    this.selectTab()
   },
 }
 </script>
