c548be77f72fbea4716d448b315cf882136e0f7d
doria0421 <441419849@qq.com>
2022-07-20 21:21:22 +0800

    popover四个方向的位置

--- src/cs/demo5.vue
+++ src/cs/demo5.vue
@@ -1,17 +1,29 @@
 <template>
   <div style="padding-top: 100px; padding-left: 100px;">
     <div style="overflow: hidden;  border: 1px solid red;">
-      <g-popover>
-        <template #content>
-          <div>popover内容</div>
+      <g-popover position="top">
+        <template slot="content">
+          <div>popover内容1-top-topaskdjsakdjsakjdslkjdlskjdlsjkdlskjdlsjkdlskjdlsajkdlajkdlkajdlksajdladjk</div>
         </template>
-        <button>点我</button>
+        <g-button>点我-top</g-button>
       </g-popover>
-      <g-popover>
-        <template #content>
-          <div>popover内容</div>
+      <g-popover position="bottom">
+        <template slot="content">
+          <div>askdjsakdjsakjdslkjdlskjdlsjkdlskjdlsjkdlskjdlsajkdlajkdlkajdlksajdladjk</div>
         </template>
-        <button>点我</button>
+        <g-button>点我-bottom</g-button>
+      </g-popover>
+      <g-popover position="left">
+        <template slot="content">
+          <div>popover内容3-leftaskdjsakdjsakjdslkjdlskjdlsjkdlskjdlsjkdlskjdlsajkdlajkdlkajdlksajdladjk</div>
+        </template>
+        <g-button>点我-left</g-button>
+      </g-popover>
+      <g-popover position="right">
+        <template slot="content">
+          <div>popover内容-rightaskdjsakdjsakjdslkjdlskjdlsjkdlskjdlsjkdlskjdlsajkdlajkdlkajdlksajdladjk</div>
+        </template>
+        <g-button>点我-right</g-button>
       </g-popover>
     </div>
   </div>
--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="popover" ref="popover" @click="onClick">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+    <div ref="contentWrapper" class="content-wrapper" :class="{[`position-${position}`]:true}" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block;">
@@ -16,6 +16,15 @@ export default {
   name: 'GPopover',
   data() {
     return { visible: false }
+  },
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+      },
+    },
   },
   methods: {
     close() {
@@ -37,14 +46,26 @@ export default {
       }
     },
     positionContent() {
-      let contentWrapper = this.$refs.contentWrapper
-      let popover = this.$refs.popover
-      let { left, top } = popover.getBoundingClientRect()
-
+      const { contentWrapper, triggerWrapper } = this.$refs
       document.body.appendChild(contentWrapper)
-      let scroll = this.getScroll()
-      contentWrapper.style.left = `${scroll.x + left}px`
-      contentWrapper.style.top = `${scroll.y + top}px`
+
+      let { width, height, left, top } = triggerWrapper.getBoundingClientRect()
+      let { height: height2 } = contentWrapper.getBoundingClientRect()
+      let { x, y } = this.getScroll()
+
+      if (this.position === 'top') {
+        contentWrapper.style.left = x + left + 'px'
+        contentWrapper.style.top = y + top + 'px'
+      } else if (this.position === 'bottom') {
+        contentWrapper.style.left = x + left + 'px'
+        contentWrapper.style.top = y + top + height + 'px'
+      } else if (this.position === 'left') {
+        contentWrapper.style.left = x + left + 'px'
+        contentWrapper.style.top = y + top + (height - height2) / 2 + 'px'
+      } else if (this.position === 'right') {
+        contentWrapper.style.left = x + left + width + 'px'
+        contentWrapper.style.top = y + top + (height - height2) / 2 + 'px'
+      }
     },
     onClick(ev) {
       let bool = this.$refs.triggerWrapper.contains(ev.target)
@@ -95,13 +116,12 @@ $border-radius: 4px;
   /* box-shadow: 0 0 3px rgba(0, 0, 0, 0.5); */
   filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
   background: white;
-  transform: translateY(-100%);
-  margin-top: -10px;
+
   padding: 0.5em 1em;
   max-width: 20em;
   border: 1px solid $border-color;
   border-radius: $border-radius;
-
+  word-break: break-all;
   &::before,
   &::after {
     content: '';
@@ -110,15 +130,71 @@ $border-radius: 4px;
     width: 0;
     height: 0;
     position: absolute;
-    left: 10px;
   }
-  &::before {
-    border-top-color: $border-color;
-    top: 100%;
+
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -10px;
+    &::before,
+    &::after {
+      left: 10px;
+    }
+    &::before {
+      border-top-color: $border-color;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
   }
-  &::after {
-    border-top-color: white;
-    top: calc(100% - 1px);
+  &.position-bottom {
+    margin-top: 10px;
+    &::before,
+    &::after {
+      left: 10px;
+    }
+    &::before {
+      border-bottom-color: $border-color;
+      bottom: 100%;
+    }
+    &::after {
+      border-bottom-color: #fff;
+      bottom: calc(100% - 1px);
+    }
+  }
+  &.position-left {
+    transform: translateX(-100%);
+    margin-left: -10px;
+    &::before,
+    &::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      border-left-color:  $border-color;
+      left: 100%;
+    }
+    &::after {
+      border-left-color: white;
+      left: calc(100% - 1px);
+    }
+  }
+  &.position-right {
+    margin-left: 10px;
+    &::before,
+    &::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      border-right-color:  $border-color;
+      right: 100%;
+    }
+    &::after {
+      border-right-color: white;
+      right: calc(100% - 1px);
+    }
   }
 }
 </style> 
