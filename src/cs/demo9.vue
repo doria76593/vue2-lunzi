107cc82706181374164085054d20a560771f1f53
doria76593 <441419849@qq.com>
2022-07-26 15:53:20 +0800

    init sub-nav

--- src/cs/demo9.vue
+++ src/cs/demo9.vue
@@ -1,9 +1,30 @@
 <template>
   <div>
-    <h2>{{selected}}</h2>
-    <g-nav :selected.sync="selected">
+    <!-- <h2>{{selected}}</h2> -->
+    <!-- <g-nav :selected.sync="selected">
       <g-nav-item name="home">首页</g-nav-item>
       <g-nav-item name="about">关于</g-nav-item>
+      <g-nav-item name="hire">招聘</g-nav-item>
+    </g-nav> -->
+    <h2>demo2--{{selected}}--支持任意层级</h2>
+    <g-nav :selected.sync="selected">
+      <g-nav-item name="home">首页</g-nav-item>
+      <g-sub-nav>
+        <template #title>关于</template>
+        <g-nav-item name="culture">企业文化</g-nav-item>
+        <g-nav-item name="developers">开发团队</g-nav-item>
+        <g-sub-nav>
+          <template #title>联系方式</template>
+          <g-nav-item name="wechat">微信</g-nav-item>
+          <g-nav-item name="qq">QQ</g-nav-item>
+          <g-sub-nav>
+            <template #title>手机</template>
+            <g-nav-item name="cm">移动</g-nav-item>
+            <g-nav-item name="cu">联通</g-nav-item>
+            <g-nav-item name="cn">电信</g-nav-item>
+          </g-sub-nav>
+        </g-sub-nav>
+      </g-sub-nav>
       <g-nav-item name="hire">招聘</g-nav-item>
     </g-nav>
   </div>
@@ -13,12 +34,10 @@ export default {
   components: {},
   data() {
     return {
-      selected: 'about',
+      selected: 'culture',
     }
   },
-  methods: {
-
-  },
+  methods: {},
 }
 </script>
 <style scoped lang="scss">
