a77b4a3e1c46e4bf6d9ae8d222304b5c43efc8b0
doria76593 <441419849@qq.com>
2022-07-28 09:14:19 +0800

    解决剩下两个bug

--- src/cs/demo11.vue
+++ src/cs/demo11.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <!-- <div class="content1">content1</div> -->
+    <div class="content1">content1</div>
     <g-sticky>
-      <div class="one">one</div>
+      <div class="one">
+        <img
+          src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.alicdn.com%2Fimgextra%2Fi3%2F365866548%2FTB2OZpennvI8KJjSspjXXcgjXXa_%21%21365866548.jpg&refer=http%3A%2F%2Fimg.alicdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1661507844&t=e9fc9d2acfcc3b05fead7ab3a27d08ac"
+          alt="" />
+      </div>
     </g-sticky>
     <div class="content2">
       <p>1</p>
@@ -15,6 +19,7 @@
       <p>8</p>
       <p>9</p>
     </div>
+    <div class="content3"></div>
   </div>
 </template>
 <script>
@@ -26,19 +31,27 @@ export default {
 }
 </script>
 <style scoped lang="scss">
+img{
+  max-width: 100%;
+}
 .content1 {
   height: 800px;
   background: yellow;
 }
+.content3 {
+  height: 1800px;
+  background: yellowgreen;
+}
 .content2 {
   /* height: 1200px; */
   background: pink;
-  >p{
+  > p {
     height: 30px;
   }
 }
 .one {
-  height: 100px;
+  width: 800px;
+  margin: 0 auto;
   background: rgb(148, 135, 135);
   border: 1px solid red;
 }
--- src/sticky.vue
+++ src/sticky.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="gulu-sticky-wrapper" ref="wrapper">
-    <div class="gulu-sticky" :class="{sticky}">
+  <div class="gulu-sticky-wrapper" ref="wrapper" :style="{height}">
+    <div class="gulu-sticky" ref="wrapper2" :class="{sticky}" :style="{left,width}">
       <slot></slot>
     </div>
   </div>
@@ -15,13 +15,23 @@ export default {
     }
   },
   mounted() {
-    let { top, height } = this.offsetTop()//注意点1 元素距离顶部的距离只能获取一次，不然会有bug
-    console.log('top', top, height)
-    this.$refs.wrapper.style.height = height + 'px'//bug2:用自己的高度给自己，让自己占位，里面的元素去fix
+    //bug1 元素距离顶部的距离只能获取一次，不然会有bug
+    //bug2:用自己的高度给自己，让自己占位，里面的元素去fix
+    // bug3:图片资源可能延迟加载，到时获取的高度不正确
+    // bug4:要动态设置left和width
+    let { top } = this.offsetTop()
+    // this.$refs.wrapper.style.height = height + 'px'
     window.addEventListener('scroll', () => {
       let scrollTop = this.getScroll().scrollTop
       console.log(`top:${top}---scrollTop:${scrollTop}`)
+
       if (scrollTop > top) {
+        let { left, width } = this.$refs.wrapper.getBoundingClientRect()
+        let { height } = this.$refs.wrapper2.getBoundingClientRect()
+        console.log(`height:${height}---left:${left}---width:${width}`)
+        this.height = height + 'px'
+        this.left = left + 'px'
+        this.with = width + 'px'
         console.log('sticky了')
         this.sticky = true
       } else {
@@ -32,14 +42,17 @@ export default {
   },
   data() {
     return {
-      sticky: false
+      sticky: false,
+      height: undefined,
+      left: undefined,
+      width: undefined,
     }
   },
   methods: {
     offsetTop() {
-      let { top, height } = this.$refs.wrapper.getBoundingClientRect()
+      let { top } = this.$refs.wrapper.getBoundingClientRect()
       let scrollTop = this.getScroll().scrollTop
-      return { top: top + scrollTop, height }
+      return { top: top + scrollTop }
     },
     getScroll() {
       var scrollLeft = window.pageXOffset !== undefined ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft
