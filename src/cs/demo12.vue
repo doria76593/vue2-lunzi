883d22a9cce9a7be3632aff8cd8e720e00d2709e
doria76593 <441419849@qq.com>
2022-07-28 22:47:12 +0800

    table

--- src/cs/demo12.vue
+++ src/cs/demo12.vue
@@ -2,11 +2,17 @@
   <div>
     <h2>表格1--{{orderBy}}</h2>
     <div style="margin: 20px;">
-      <g-table :columns="columns" :data-source="dataSource" bordered :selectedItems.sync="selected" :order-by.sync="orderBy" @update:orderBy="x" :loading="loading"></g-table>
+      <g-table :columns="columns" :data-source="dataSource" bordered :selected-items.sync="selected" :order-by.sync="orderBy" @update:orderBy="x" :loading="loading" :height="400"
+        expend-field="description" checkable>
+        <template slot-scope="xxx">
+          <button @click="edit(xxx.item)">编辑</button>
+          <button @click="view(xxx.item)">查看</button>
+          <button>删除</button>
+        </template>
+      </g-table>
     </div>
-    <h2>表格2</h2>
     <div style="margin: 20px;">
-      <g-table :columns="columns" :data-source="dataSource" numberVisible compact :striped="false" :loading="loading"></g-table>
+      <g-table :columns="columns" :data-source="dataSource" bordered compact :striped="false"></g-table>
     </div>
   </div>
 </template>
@@ -16,23 +22,35 @@ export default {
     return {
       selected: [],
       columns: [
-        { text: '姓名', field: 'name' },
+        { text: '姓名', field: 'name', width: 100 }, // 1
         { text: '分数', field: 'score' },
       ],
       orderBy: { // true - 开启排序，但是不确定asc desc
         score: 'desc'
       },
+      loading: false,
       dataSource: [
-        { id: 1, name: '方方', score: 100 },
-        { id: 2, name: '圆圆', score: 99 },
+        { id: 1, name: '方方', score: 100, description: 'xxxx xxxx' },
+        { id: 2, name: '圆圆', score: 99, description: 'yyyy yyyy' },
         { id: 3, name: '张三', score: 100 },
         { id: 4, name: '李四', score: 99 },
         { id: 5, name: '超人', score: 100 },
         { id: 6, name: '蝙蝠侠', score: 99 },
         { id: 7, name: '蜘蛛侠', score: 100 },
         { id: 8, name: '钢铁侠', score: 99 },
-      ],
-      loading: false,
+        { id: 9, name: '方方', score: 100 },
+        { id: 10, name: '圆圆', score: 99 },
+        { id: 11, name: '张三', score: 100 },
+        { id: 12, name: '李四', score: 99 },
+        { id: 13, name: '超人', score: 100 },
+        { id: 14, name: '蝙蝠侠', score: 99 },
+        { id: 15, name: '蜘蛛侠', score: 100 },
+        { id: 16, name: '钢铁侠', score: 99 },
+        { id: 17, name: '蜘蛛侠', score: 100 },
+        { id: 18, name: '钢铁侠', score: 99 },
+        { id: 19, name: '方方', score: 100 },
+        { id: 20, name: '圆圆', score: 99 },
+      ]
     }
   },
   methods: {
@@ -42,7 +60,13 @@ export default {
         this.dataSource = this.dataSource.sort((a, b) => a.score - b.score)
         this.loading = false
       }, 3000)
-    }
+    },
+    edit(item) {
+      alert(`开始编辑${item.id}`)
+    },
+    view(item) {
+      alert(`开始查看${item.id}`)
+    },
   },
 }
 </script>
